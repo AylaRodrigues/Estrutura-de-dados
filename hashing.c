1a22b41bb9579d7293cd316c2d4d7cd805a388bc
Douglas Mareli <82533319+DouglasMreli@users.noreply.github.com>
2021-07-16 08:23:30 -0300

    Add files via upload

---
+++ hashing.c
@@ -0,0 +1,69 @@
+// Grupo: Ayla Rodrigues, Douglas Mareli, Sofia Kitaeva
+// Exercicio de Hashing - Estrutura de dados
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+typedef struct registro {
+	
+	int mat;
+	char nome[20];
+	char curso[50];
+	int disponibilidade;
+	
+}registro;
+
+int hash(int key, int size) {
+	return key % size;
+}
+
+void inicializar(char *nomeArq) {
+	FILE *arq = fopen(nomeArq, "wb");
+	registro a;
+	for(int i = 0; i < 20; i++) {
+		fwrite(&a, sizeof(registro), 1, arq);
+	}
+	
+	fclose(arq);
+}
+
+int main(){
+	setlocale(LC_ALL, "");
+	
+	int opcao = 0;
+	
+	inicializar("alunos.bin");
+	
+	while(opcao!=4)
+	{
+		printf("O que deseja fazer?\n 1.Inserir novo aluno\n 2.Impremir as informações de um determinado aluno\n 3.Imprimir tabela Hash\n 4.Sair\n>");
+		scanf("%d", &opcao);
+		printf("\n");
+		
+		if (opcao==1)//inserir elemento
+		{
+			
+		}
+		else if(opcao==2)//remover elemento
+		{
+			
+			
+		}
+		else if(opcao==3)//imprimir heap
+		{
+			
+		}
+		else if(opcao == 4) {
+			opcao = 4;
+		}
+		
+		printf("\n\n");
+		system("pause");
+		system("cls");
+	}
+		
+		printf("O usuário saiu!\n");
+		
+	return 0;
+}
