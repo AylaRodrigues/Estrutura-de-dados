55c205c6075e6ac1855a163439f1ec22ddb98f8f
Douglas Mareli <82533319+DouglasMreli@users.noreply.github.com>
2021-07-18 21:27:43 -0300

    Update hashing.c

--- hashing.c
+++ hashing.c
@@ -6,7 +6,6 @@
 #include <locale.h>
 #include <string.h>
 
-#define N 11
 
 typedef struct registro {
 	
@@ -24,10 +23,10 @@ int hash(int key, int size)
 
 void inicializar(char *nomeArq) 
 {
-	FILE *arq = fopen("alunos.bin", "wb");
+	FILE *arq = fopen(nomeArq, "wb");
 	Registro a;
-	a.disponibilidade=1;
-	for(int i = 0; i < 20; i++) {
+	a.disponibilidade = 1;
+	for(int i = 0; i < 15; i++) {
 		fwrite(&a, sizeof(Registro), 1, arq);
 	}
 	
@@ -36,73 +35,94 @@ void inicializar(char *nomeArq)
 
 int AcharPosicao(char *nomeArq, int mat)
 {
-	int pos = hash(mat, N);
+	int N = 15;
+	int pos = hash(mat, 15);
 
 	Registro a;
-	FILE *arq=fopen("alunos.bin","rb");
+	FILE *arq=fopen(nomeArq,"rb");
 	fseek(arq, pos*sizeof(Registro),SEEK_SET);
 	fread(&a,sizeof(Registro), 1, arq);
 
 	while(a.disponibilidade == 0)
 	{
-		pos=(pos+1)%N;
-		fseek(arq, pos*sizeof(Registro),SEEK_SET);
+		pos = (pos+1)%N;
+		fseek(arq, pos*sizeof(Registro), SEEK_SET);
 		fread(&a,sizeof(Registro), 1, arq);
 	}
+	
 	fclose(arq);
 	return pos;
 }
 
+void imprimirAluno(char *nomeArq, int mat) {
+	
+	int N = 15;
+	FILE *arq = fopen(nomeArq,"rb");
+	
+	Registro a;
+	int pos = hash(mat, N);
+	
+	fseek(arq, pos*sizeof(Registro),SEEK_SET);
+	fread(&a,sizeof(Registro), 1, arq);
+	if(a.disponibilidade == 0 && a.mat != mat) {
+		while(a.disponibilidade != 1 || a.mat != mat) {
+			pos = (pos + 1) % N;
+			fseek(arq, pos*sizeof(Registro),SEEK_SET);
+			fread(&a, sizeof(Registro), 1, arq);
+		}
+	}
+	
+	if(a.disponibilidade == 1) {
+		printf("Esse matrícula não está no Hashing");
+	} else {
+		printf("\n\tInformações do Aluno:\n\n");
+		printf("Matrícula: %d\n",a.mat);
+		printf("Nome: %s \n",a.nome);
+		printf("Curso: %s\n",a.curso);
+	}
+	
+	fclose(arq);
+	
+}
+
 void inserir(char *nomeArq, int mat, char *nome, char *curso) 
 {
-	int pos = AcharPosicao("alunos.bin", mat);
+	int pos = AcharPosicao(nomeArq, mat);
 
-	FILE *arq = fopen("alunos.bin", "r+b");
+	FILE *arq = fopen(nomeArq, "r+b");
 
 	Registro a;
 	a.mat = mat;
 	strcpy(a.nome, nome);
 	strcpy(a.curso, curso);
-	a.disponibilidade=0;
+	a.disponibilidade = 0;
 
 	fseek(arq, pos*sizeof(Registro), SEEK_SET);
 	fwrite(&a, sizeof(Registro), 1, arq);
-	printf("Aluno inserido com sucesso\n\n");
+	
 
 	fclose(arq);
-	Sleep(1000);
-	system("cls");
 }
 
-void buscarAluno(char *nome)
-{
-	FILE *arquivo;
-
-	Registro registro;
-
-	arquivo=fopen("alunos.bin", "rb");
-
-	if (arquivo == NULL)
-	{
-		printf("Problemas na leitura do arquivo\n");
-	}
-	else
-	{
-		while(fread(&registro, sizeof(Registro), 1, arquivo)==1)
-		{
-			if(nome == registro.nome)
-			{
-				//nao chega ate aqui
-				printf("> Nome do aluno : %s\n", nome);
-				printf("> Matricula do aluno : %s\n", registro.mat);
-				printf("> Curso do aluno : %s\n", registro.curso);
-			}
+void ImprimirHashing(char *nomeArq) {
+	
+	int N = 15;
+	FILE *arq = fopen(nomeArq,"rb");
+	
+	Registro a;
+	
+	for(int pos = 0; pos < N; pos++) {
+		fseek(arq, pos * sizeof(Registro), SEEK_SET);
+		fread(&a, sizeof(Registro), 1, arq);
+		if(a.disponibilidade == 0) {
+			printf("\n\tInformações do Aluno:\n\n");
+			printf("Matrícula: %d\n",a.mat);
+			printf("Nome: %s \n",a.nome);
+			printf("Curso: %s\n",a.curso);
 		}
 	}
-
-	Sleep(1000);
-	system("cls");
-	fclose(arquivo);
+	
+	fclose(arq);
 }
 
 int main()
@@ -113,8 +133,9 @@ int main()
 	int matricula;
 	char nome[20];
 	char curso[50];
+	char arq[10] = "alunos.bin";
 	
-	inicializar("alunos.bin");
+	inicializar(arq);
 	
 	while(opcao!=4)
 	{
@@ -122,33 +143,46 @@ int main()
 		scanf("%d", &opcao);
 		printf("\n");
 		
-		if (opcao==1)//inserir novo aluno
+		if (opcao == 1)//inserir novo aluno
 		{
 			printf("> Qual a matricula do aluno?\n");
 			scanf("%d", &matricula);
-
+	
+			fflush(stdin);
 			printf("> Qual o nome do aluno?\n");
-			scanf("%s", &nome);
-
-			printf("> Qual o curso do aluno?\n");
-			scanf("%s", &curso);
-
-			inserir("alunos.bin", matricula, nome, curso);
-
-		}
-		else if(opcao==2)//imprimir info
-		{
-			printf("> Qual o nome do aluno?\n");
-			scanf("%s", &nome);
-
-			buscarAluno(nome);
+			scanf("%[^\n]s", nome);
+			fflush(stdin);
 			
+			printf("> Qual o curso do aluno?\n");
+			scanf("%[^\n]s", curso);
+			fflush(stdin);
+			
+			inserir(arq, matricula, nome, curso);
+			
+			printf("\n-------------\n");
+			printf("Aluno Inserido");
+			printf("\n-------------\n");
+		}
+		else if(opcao == 2)//imprimir info
+		{
+			int mat, pos;
+			printf("> Qual o matrícula do aluno?\n");
+			scanf("%d", &mat);
+			
+			printf("\n-------------------------------\n");
+			imprimirAluno(arq, mat);
+			printf("\n-------------------------------\n");
 		}
 		else if(opcao==3)//imprimir tabela de dispersao
 		{
-			
+			printf("\n-------------------------------\n");
+			ImprimirHashing(arq);
+			printf("\n-------------------------------\n");
 		}
+		
 		printf("\n\n");
+		
+		system("pause");
 		system("cls");
 	}
 	printf("O usuário saiu!\n");
