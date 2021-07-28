0cff19318fb3efcba5fe0440397cd84e3751dd43
Ayla Rodrigues <72139332+AylaRodrigues@users.noreply.github.com>
2021-07-28 19:19:24 -0300

    Update Backtracking3.c

--- Backtracking3.c
+++ Backtracking3.c
@@ -6,17 +6,24 @@ void ImprimirCombinacoes(int *vet, int pos, int cent, int* vetAux){
 	int aux=0;
 	
 	if(pos==cent){
-		for(int i=0; i<=cent;i++)
+		for(int i=0; i<cent;i++)
 			aux+=vet[i];
-		if(aux==cent){
-		for(int i=0; i<=cent;i++)
-			if(vet[i]!=0)
-				printf("%d ", vet[i]);
-			printf("\n");
+		if(aux==cent)
+		{
+			for(int i=0; i<cent;i++)
+			{
+				if(vet[i]!=0)
+				{
+					printf("%d ", vet[i]);
+				}
+				
+			}printf("\n");
+
 		}
 	}
 	else{
-		for(int i=0;i<=3;i++){
+		for(int i=0;i<=5;i++)
+		{
 			vet[pos]=vetAux[i];
 			ImprimirCombinacoes(vet,pos+1,cent,vetAux);
 		}
@@ -25,10 +32,10 @@ void ImprimirCombinacoes(int *vet, int pos, int cent, int* vetAux){
 
 int main(){
 	int cent;
-	
+
 	printf("Quantos centavos quer representar?\n>> ");
 	scanf("%d", &cent);
-	
+
 	int* vet = (int*) malloc(sizeof(int)*cent);
 	int* vetAux = (int*) malloc(sizeof(int)*6);	
 	vetAux[0] = 1;
@@ -37,8 +44,8 @@ int main(){
 	vetAux[3] = 25;
 	vetAux[4] = 50;
 	vetAux[5] = 0;
-	
+
 	ImprimirCombinacoes(vet,0,cent,vetAux);
-	
+
 	return 0;
 }
